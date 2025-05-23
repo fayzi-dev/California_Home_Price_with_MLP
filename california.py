b1d8ba761f78477921e1dcffac4366543ba7f24f
Mohammad Fayzi <fayzi.dev@gmail.com>
2025-05-23 08:34:13 +0330

    Define loop model

--- california.py
+++ california.py
@@ -4,7 +4,6 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from torch.utils.data import DataLoader, TensorDataset
 
-
 data_train = pd.read_csv('train.csv')
 x_train = data_train.drop('MedHouseVal', axis=1)
 # print(x_train.head())
@@ -29,32 +28,31 @@ print(y_test.shape)  # torch.Size([4128])
 mu = x_train.mean(dim=0)
 std = x_train.std(dim=0)
 x_train = (x_train - mu) / std
-print(x_train.mean(dim=0))#tensor([ 1.6383e-07, -1.3862e-09, -5.4984e-08,  8.3169e-08, -1.7188e-07,
-         # 1.4555e-08, -1.5668e-06,  1.3101e-06])
+print(x_train.mean(dim=0))  # tensor([ 1.6383e-07, -1.3862e-09, -5.4984e-08,  8.3169e-08, -1.7188e-07,
+# 1.4555e-08, -1.5668e-06,  1.3101e-06])
 print(x_train.std(dim=0))
 # tensor([1.0000, 1.0000, 1.0000, 1.0000, 1.0000, 1.0000, 1.0000, 1.0000])
 
 x_test = (x_test - mu) / std
-print(x_test.mean(dim=0))#tensor([ 0.0174,  0.0059,  0.0357,  0.0232, -0.0412, -0.0045,  0.0125, -0.0141])
-print(x_test.std(dim=0))#tensor([1.0173, 0.9952, 1.3752, 1.2463, 0.8829, 0.7154, 0.9926, 0.9963])
+print(x_test.mean(dim=0))  # tensor([ 0.0174,  0.0059,  0.0357,  0.0232, -0.0412, -0.0045,  0.0125, -0.0141])
+print(x_test.std(dim=0))  # tensor([1.0173, 0.9952, 1.3752, 1.2463, 0.8829, 0.7154, 0.9926, 0.9963])
 
-#DataLoader
+# DataLoader
 dataset_train = TensorDataset(x_train, y_train)
-print(dataset_train)#<torch.utils.data.dataset.TensorDataset object at 0x000001A424673520>
+print(dataset_train)  # <torch.utils.data.dataset.TensorDataset object at 0x000001A424673520>
 # print(dataset_train.tensors[0])
 
 Dataloader_train = DataLoader(dataset_train, batch_size=150, shuffle=True)
-print(Dataloader_train)#<torch.utils.data.dataloader.DataLoader object at 0x000001678B9B8730>
+print(Dataloader_train)  # <torch.utils.data.dataloader.DataLoader object at 0x000001678B9B8730>
 
 # for x_batch, y_batch in Dataloader_train:
 #     print(x_batch.shape,y_batch.shape)#torch.Size([150, 8]) torch.Size([150])
 #     break
 
-for i,(x_batch, y_batch) in enumerate(Dataloader_train):
-    print(i,x_batch.shape,y_batch.shape)
+for i, (x_batch, y_batch) in enumerate(Dataloader_train):
+    print(i, x_batch.shape, y_batch.shape)
 
-print(Dataloader_train.__len__())#111
-
+print(Dataloader_train.__len__())  # 111
 
 # x_b , y_b =next(iter(Dataloader_train))
 # print(x_b.shape,y_b.shape)
@@ -66,7 +64,7 @@ Dataloader_test = DataLoader(dataset_test, batch_size=300, shuffle=False)
 # print(Dataloader_test)
 
 
-#Model Define
+# Model Define
 num_features = 8
 out_features = 1
 
@@ -81,3 +79,27 @@ model = nn.Sequential(nn.Linear(num_features, 64),
 # Loss & Optimizer
 loss_function = nn.MSELoss()
 optimizer = optim.SGD(model.parameters(), lr=0.001)
+
+# My Train Loop
+num_epochs = 150
+for epoch in range(num_epochs):
+    loss_train = 0
+    for x_batch, y_batch in Dataloader_train:
+        yp = model(x_batch)
+        loss = loss_function(yp.squeeze(), y_batch)
+        loss_train += loss
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+
+    print(f'Epoch:{epoch} : Loss_Train :{loss_train / len(Dataloader_train)}')
+
+    loss_test = 0
+    for x_batch, y_batch in Dataloader_test:
+        yp = model(x_batch)
+        loss =loss_function(yp.squeeze(), y_batch)
+        loss_test += loss
+
+    print(f'Epoch:{epoch} : Loss_Test :{loss_test / len(Dataloader_test)}')
+
+
